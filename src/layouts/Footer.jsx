27af19c26b68bae9c892121836b050bead821679
satishsahu4499 <Satish@sovtechnologies.com>
2025-10-23 11:31:11 +0530

    correct url account page
    correct url account page

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -87,7 +87,7 @@ function Footer() {
                       <Link to="/CorporateBooking">Corporate Booking</Link>
                     </li>
                      <li>
-                      <Link to="/AccountDeactivate">Account Deactivate</Link>
+                      <Link to="/account-deactivate">Account Deactivate</Link>
                     </li>
                   </ul>
                 </div>
