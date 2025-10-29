34ca1b366d602a87a7282574c02a58c2bcdf3313
satishsahu4499 <Satish@sovtechnologies.com>
2025-10-29 13:36:04 +0530

    css fiexd
    css fiexd

--- src/features/withoutauth/components/PriceChart.jsx
+++ src/features/withoutauth/components/PriceChart.jsx
@@ -148,7 +148,7 @@ function PriceChart({ venueId, sportId }) {
           
           <div className="columns row g-3">
             {transformedPriceData.map((column) => (
-              <div key={column.title} className="column col-lg-3 col-md-6 col-12 border-bottom-1 p-0">
+              <div key={column.title} className="column col-lg-3 col-md-6 col-12 border-bottom-1 pe-0">
                 <h5>{column.title}</h5>
                 {Object.entries(column.slotsByDay || {}).map(([day, slots]) => (
                   <div key={day} className="day-group">
